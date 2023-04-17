889aa20cef6547408740b21317e54ce3d13687ed
osm1nozhek <129457950+osm1nozhek@users.noreply.github.com>
2023-04-17 05:20:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+import csv
+from statistics import mean
+
+import flask
+import requests
+from faker import Faker
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+
+@app.route("/requirements/")
+def task1():
+    with open("requirements.txt", "r") as fi:
+        requirements = fi.read()
+    return flask.render_template("TASK1.html", requirements=requirements)
+
+
+@app.route("/users/generate", methods=["GET"])
+def task2():
+    dict1 = {}
+    fake = Faker()
+    num = 100
+    try:
+        num = int(flask.request.args['num'])
+    except(Exception,):
+        pass
+
+    for i in range(num):
+        dict1[fake.name()] = fake.ascii_company_email()
+    return flask.render_template("TASK2.html", dict1=dict1, num=num)
+
+
+@app.route("/mean/")
+def task3():
+    with open("hw.csv") as fi:
+        reader = list(csv.reader(fi))
+        reader.pop(0)
+        Height_Average = mean([float(reader[i][1]) for i in range(len(reader) - 1)]) * 2.54
+        Weight_Average = mean([float(reader[i][2]) for i in range(len(reader) - 1)]) / 2.205
+
+    return flask.render_template("TASK3.html", Height_Average=Height_Average, Weight_Average=Weight_Average)
+
+
+@app.route("/space/")
+def task4():
+    request = requests.get("http://api.open-notify.org/astros.json")
+    number = request.json()["number"]
+    return flask.render_template("TASK4.html", number=number)
+
+
+if __name__ == '__main__':
+    app.run()
